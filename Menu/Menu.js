9d58092fae3a77bcf118255b1e669b283b1cd8ba
Kirsten S <kirsten@Kirstens-MacBook-Air.local>
2020-04-14 20:49:43 -0400

    created function for menu.js, created menu div and ul element

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -33,3 +33,30 @@ let menuItems = [
   Step 6: add the menu component to the DOM.
   
 */
+
+//My code
+//pseudocode
+  //create menu div
+  //add class to menu div
+  //create ul element
+  //create li elements by iterating over array
+  //add li elements to ul element as children
+  //add ul element to menu div as child
+
+/* component structure
+  <div class="menu">
+    <ul>
+      {each menu item as a list item}
+    </ul>
+  </div>
+*/
+
+function menuComponentConstructor(menuDataArr) {
+  //create menu(outer) div
+  const menu = document.createElement('div');
+
+  //create ul element
+  const menuListContainer = document.createElement('ul');
+
+  
+}
